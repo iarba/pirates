a7f7df0e72ff3b9b1d25cc5319f70afdffa17630
iarba <dociarba@gmail.com>
2020-04-15 00:49:34 +0100

    added slicing procedure

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include "log.h"
+#include "model/sea.h"
+#include "controller/slicer.h"
+
+#define forever while(true)
 
 int main()
 {
   log_init("server");
+  sea *s = new sea();
+  forever
+  {
+    slicer.tick(s);
+  }
+  delete s;
   return 0;
 }
