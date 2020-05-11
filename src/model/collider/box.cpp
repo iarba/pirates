596ce677dbaf6f7e9622b4ed094dc906ebb24398
iarba <dociarba@gmail.com>
2020-05-11 01:06:22 +0100

    made collider box rotate around offset

--- src/model/collider/box.cpp
+++ src/model/collider/box.cpp
@@ -11,9 +11,9 @@ std::vector<glm::dvec2> collider_box::get_points()
 {
   std::vector<glm::dvec2> ret;
   glm::dmat2x2 rmat = get_rotation_matrix(pp.angle);
-  ret.push_back(rmat * glm::dvec2({-x / 2, -z / 2}) + pp.position);
-  ret.push_back(rmat * glm::dvec2({-x / 2,  z / 2}) + pp.position);
-  ret.push_back(rmat * glm::dvec2({ x / 2,  z / 2}) + pp.position);
-  ret.push_back(rmat * glm::dvec2({ x / 2, -z / 2}) + pp.position);
+  ret.push_back(rmat * (glm::dvec2({-x / 2, -z / 2}) - pp.offset) + pp.position + pp.offset);
+  ret.push_back(rmat * (glm::dvec2({-x / 2,  z / 2}) - pp.offset) + pp.position + pp.offset);
+  ret.push_back(rmat * (glm::dvec2({ x / 2,  z / 2}) - pp.offset) + pp.position + pp.offset);
+  ret.push_back(rmat * (glm::dvec2({ x / 2, -z / 2}) - pp.offset) + pp.position + pp.offset);
   return ret;
 }
